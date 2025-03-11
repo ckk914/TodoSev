ad3d4f0d9cb39a6fc2a96fcbe8413911367fa1f3
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-03-12 08:11:27 +0900

    chore: main boot setting

--- src/main/java/com/kon/Main.java
+++ src/main/java/com/kon/Main.java
@@ -1,10 +1,16 @@
 package com.kon;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
+
+        SpringApplication.run(Main.class, args); //
         // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
         System.out.printf("Hello and welcome!🌟");
 
