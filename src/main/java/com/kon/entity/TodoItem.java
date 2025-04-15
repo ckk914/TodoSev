9841ec3f6326fb8eb76289873f850524d7ffd91e
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-04-15 23:24:03 +0900

    add : todoItem.java 내용 추가

--- src/main/java/com/kon/entity/TodoItem.java
+++ src/main/java/com/kon/entity/TodoItem.java
@@ -1,4 +1,17 @@
 package com.kon.entity;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+//to-do list 설계
+@Data
 public class TodoItem {
+    private Long id;
+    private String userId; //User와 연관관계 (FK)
+    private String title;
+    private String description;
+    private boolean completed;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
 }
